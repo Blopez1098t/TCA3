81af45b52b4cc05f651df9758132faf7b127d808
Bryan <125blopez@frhsd.com>
2024-03-27 11:00:45 -0400

    finished

---
+++ js/script.js
@@ -0,0 +1,120 @@
+function one(){
+    document.getElementById('output').innerHTML = "There are over 20,000 bee species worldwide, including the honey bee, which originated in Eurasia and has been imported around the globe as a domesticated species. Wild bees species live on every continent except Antarctica. In North America there are approximately 4,000 native bee species occupying ecosystems from forests to deserts to grasslands.";
+}
+
+function two(){
+    document.getElementById('title').innerHTML = "Range";
+}
+
+function three(){
+    document.getElementById('img').src="img/2nd.jpg"
+}
+
+function one1(){
+    document.getElementById('output').innerHTML = "As they forage, bees perform the critical act of pollination. As a bee enters a flower to feed on nectar and gather pollen, some of the pollen sticks to the bee’s body. When the bee flies on, it deposits some of that pollen on the next flower it visits, resulting in fertilization, allowing the plant to reproduce and to generate the fruits and seeds so many other wildlife species rely on as a food source. In fact, bees pollinate a staggering 80 percent of all flowering plants, including approximately 75 percent of the fruits, nuts, and vegetables grown in the United States.";
+}
+
+function two1(){
+    document.getElementById('title').innerHTML = "Behavior";
+}
+
+function three1(){
+    document.getElementById('img').src="img/3rd.jpg"
+}
+
+function one2(){
+    document.getElementById('output').innerHTML = "Bees begin life as eggs, which hatch into larvae the feed and pupate and eventually emerge in their adult form, where they visit flowers to feed on and gather nectar and pollen. Unlike the hive-forming domesticated honey bee or wild bumble bee species, most bees are solitary nesters. They don’t form hives, create honey, or live a communal lifestyle. Instead, they lay their eggs in a series of tiny chambers in tunnels in the ground, in hollow plant stems, or in decaying wood.";
+}
+
+function two2(){
+    document.getElementById('title').innerHTML = "Life History";
+}
+
+function three2(){
+    document.getElementById('img').src="img/4th.png"
+}
+
+function one3(){
+    document.getElementById('output').innerHTML = "Both domesticated honey bees and many native bee species are in decline. In fact some species, such as the once-common rusty patched bumblebee, are now listed as endangered in the U.S. Potential causes of these declines include habitat destruction, disease, agricultural and lawn and garden practices, use of pesticides, habitat fragmentation, changes in land use, invasive species, and climate change. Pollinators, notably native bees, are critical to the survival of North America’s native plants, other wildlife, and people alike.";
+}
+
+function two3(){
+    document.getElementById('title').innerHTML = "Conservation";
+}
+
+function three3(){
+    document.getElementById('img').src="img/5th.jpg"
+}
+
+function one4(){
+    document.getElementById('output').innerHTML = "Some species of bee employ a technique called sonication, or buzz pollination, when they harvest pollen. During sonication, the bee rapidly vibrates its flight muscles while attached to the flower. This loosens the pollen, which makes it easier to collect.";
+}
+
+function two4(){
+    document.getElementById('title').innerHTML = "Fun Fact";
+}
+
+function three4(){
+    document.getElementById('img').src="img/6th.jpg"
+}
+
+function one5(){
+    document.getElementById('output').innerHTML = "Like all insects, a bee’s body is divided into three parts: a head with two antennae, a thorax with six legs, and an abdomen. All bees have branched hairs somewhere on their bodies and two pairs of wings. Only female bees have stingers. Many bee species have black and yellow coloration, but many do not—they actually come in a variety of colors, including green, blue, red, or black. Some are striped, and some even have a metallic sheen.  They range in size from large carpenter bees and bumble bees to the tiny Perdita minima bee,  which is less than two millimeters long.";
+}
+
+function two5(){
+    document.getElementById('title').innerHTML = "Bee's";
+}
+
+function three5(){
+    document.getElementById('img').src="img/1st.jpg"
+}
+
+function cbgc(){
+    document.querySelector(".but1").style.backgroundColor= "rgb(255, 199, 79)"; 
+    document.querySelector(".but2").style.backgroundColor= "black"; 
+    document.querySelector(".but3").style.backgroundColor= "rgb(255, 174, 0)"; 
+    document.querySelector(".but4").style.backgroundColor= "black"; 
+    document.querySelector(".but5").style.backgroundColor= "rgb(255, 174, 0)"; 
+    document.querySelector(".but6").style.backgroundColor= "black"; 
+}
+function cbgc2(){
+    document.querySelector(".but1").style.backgroundColor= "black"; 
+    document.querySelector(".but2").style.backgroundColor= "rgb(255, 199, 79)"; 
+    document.querySelector(".but3").style.backgroundColor= "black"; 
+    document.querySelector(".but4").style.backgroundColor= "rgb(255, 174, 0)"; 
+    document.querySelector(".but5").style.backgroundColor= "black"; 
+    document.querySelector(".but6").style.backgroundColor= "rgb(255, 174, 0)"; 
+}
+function cbgc3(){
+    document.querySelector(".but1").style.backgroundColor= "rgb(255, 174, 0)"; 
+    document.querySelector(".but2").style.backgroundColor= "black"; 
+    document.querySelector(".but3").style.backgroundColor= "rgb(255, 199, 79)"; 
+    document.querySelector(".but4").style.backgroundColor= "black"; 
+    document.querySelector(".but5").style.backgroundColor= "rgb(255, 174, 0)"; 
+    document.querySelector(".but6").style.backgroundColor= "black"; 
+}
+function cbgc4(){
+    document.querySelector(".but1").style.backgroundColor= "black"; 
+    document.querySelector(".but2").style.backgroundColor= "rgb(255, 174, 0)"; 
+    document.querySelector(".but3").style.backgroundColor= "black"; 
+    document.querySelector(".but4").style.backgroundColor= "rgb(255, 199, 79)"; 
+    document.querySelector(".but5").style.backgroundColor= "black"; 
+    document.querySelector(".but6").style.backgroundColor= "rgb(255, 174, 0)"; 
+}
+function cbgc5(){
+    document.querySelector(".but1").style.backgroundColor= "rgb(255, 174, 0)"; 
+    document.querySelector(".but2").style.backgroundColor= "black"; 
+    document.querySelector(".but3").style.backgroundColor= "rgb(255, 174, 0)"; 
+    document.querySelector(".but4").style.backgroundColor= "black"; 
+    document.querySelector(".but5").style.backgroundColor= "rgb(255, 199, 79)"; 
+    document.querySelector(".but6").style.backgroundColor= "black"; 
+}
+function cbgc6(){
+    document.querySelector(".but1").style.backgroundColor= "black"; 
+    document.querySelector(".but2").style.backgroundColor= "rgb(255, 174, 0)"; 
+    document.querySelector(".but3").style.backgroundColor= "black"; 
+    document.querySelector(".but4").style.backgroundColor= "rgb(255, 174, 0)"; 
+    document.querySelector(".but5").style.backgroundColor= "black"; 
+    document.querySelector(".but6").style.backgroundColor= "rgb(255, 199, 79)"; 
+}
